ae125fbb57432dbc918a166c1eafeb5cc5676dfb
Kishan Patel <patel99kishan@gmail.com>
2024-02-05 19:20:57 +0530

    Setup PeerJs and Create Lobby

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,35 @@
-import { useSocket } from "@/context/socket"
-import { useEffect } from "react"
+import { v4 as uuidv4 } from "uuid";
+import { useRouter } from "next/router";
+import { useState } from "react";
+
+import styles from '@/styles/home.module.css'
+
+
 
 export default function Home() {
-  const socket = useSocket()
-  useEffect(() => {
-    socket?.on("connect", () => {
-      console.log(socket.id);
-    });
-  }, [socket])
+  const router = useRouter()
+  const [roomId, setRoomId] = useState('')
+
+  const createAndJoin = () => {
+    const roomId = uuidv4()
+    router.push(`/${roomId}`)
+  }
+
+  const joinRoom = () => {
+    if (roomId) router.push(`/${roomId}`)
+    else {
+      alert("Please provide a valid room id")
+    }
+  }
   return (
-    <h1>Welcome</h1>
+    <div className={styles.homeContainer}>
+      <h1>Google Meet Clone</h1>
+      <div className={styles.enterRoom}>
+        <input placeholder='Enter Room ID' value={roomId} onChange={(e) => setRoomId(e?.target?.value)} />
+        <button onClick={joinRoom}>Join Room</button>
+      </div>
+      <span className={styles.separatorText} >--------------- OR ---------------</span>
+      <button onClick={createAndJoin}>Create a new room</button>
+    </div>
   )
 }
